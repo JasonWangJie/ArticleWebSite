eafec1bd1865a602664fb14d0d3be7036b9ae6a8
JasonWangJie <1093943862@qq.com>
2016-04-25 15:10:12 +0800

    修改路由起始页

--- Article.WebSite.Article/App_Start/RouteConfig.cs
+++ Article.WebSite.Article/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace Article.WebSite.Article
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { action = "Index", id = UrlParameter.Optional }
+                defaults: new {controller =
+"Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
